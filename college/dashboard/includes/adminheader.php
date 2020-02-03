57c44521bce9f113d6b9adfc8ed6baed00130f92
sirjoe7 <josephgachanja49@gmail.com>
2020-02-03 14:13:38 +0300

    Removed Login Requirements

--- college/dashboard/includes/adminheader.php
+++ college/dashboard/includes/adminheader.php
@@ -1,14 +1,6 @@
 <?php
 session_start();
 include('connection.php');
-if (isset($_SESSION['role'])) {
-	
-}
-else {
-    echo "<script>alert('you need to login first');
-    window.location.href='../index.php';</script>";	
-}
-
 ?>
 
 <!DOCTYPE html>
